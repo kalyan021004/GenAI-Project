c5e96eaf5144b305ef96fc1723c9e3ce78c6d6ba
kalyan021004 <kalyan021004@gmail.com>
2026-04-16 18:58:18 +0530

    changed the deployed url for frontend

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "https://gen-ai-project-n545.vercel.app",
-      "https://gen-ai-project-h3hs.vercel.app/"
+      "https://gen-ai-project-h3hs.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true
